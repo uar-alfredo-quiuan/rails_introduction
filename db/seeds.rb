a7d0fb6399cbc60e684a3dbba949c3b94e909fe2
alfredoquiuan <alfredo.quiuan@upandrunningsoftware.com>
2017-08-17 10:25:11 -0300

    #332: As a user, I want to be able to edit my profile and change my password

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.new(email: 'alfredoquiuan@admin.com', password: '123456').save(validate: false)
+p "Created the admin account"
